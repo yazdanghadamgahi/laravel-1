dfbde91b7b43967118e0fc7c8eb2cba0a098f72f
yazdanghadamgahi <yazdan.z.ghadamgahi@gmail.com>
2024-12-04 10:49:48 +0330

    add auth to web.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,28 +1,15 @@
 <x-layout>
     @slot('title')
-        <p>Register page</p>
+        <p>Login page</p>
     @endslot
     @slot('body')
-        <form method="POST" action="/register">
+        <form method="POST" action="/login">
             @csrf
 
             <div class="space-y-12">
                 <div class="border-b border-gray-900/10 pb-12">
                     <h2 class="text-base font-semibold leading-7 text-gray-900">Create a New User</h2>
                     <p class="mt-1 text-sm leading-6 text-gray-600">We just need a handful of details from you.</p>
-
-                    <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                        <div class="sm:col-span-4">
-                            <label for="username" class="block text-sm font-medium leading-6 text-gray-900">UserName</label>
-                            <div class="mt-2">
-                                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <input type="text" name="username" id="username" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6" required>
-                                </div>
-                                @error('username')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
-                                @enderror
-                            </div>
-                        </div>
 
                         <div class="sm:col-span-4">
                             <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email</label>
@@ -43,18 +30,6 @@
                                     <input type="password" name="password" id="password" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"  required>
                                 </div>
                                 @error('password')
-                                <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="sm:col-span-4">
-                            <label for="password_confirm" class="block text-sm font-medium leading-6 text-gray-900">Password Confirm</label>
-                            <div class="mt-2">
-                                <div class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                                    <input type="password" name="password_confirm" id="password_confirm" class="block flex-1 border-0 bg-transparent py-1.5 px-3 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"  required>
-                                </div>
-                                @error('password_confirm')
                                 <p class="text-xs text-red-500 font-semibold mt-1">{{$message}}</p>
                                 @enderror
                             </div>
@@ -81,4 +56,5 @@
         {{--        @endif--}}
     @endslot
 </x-layout>
+
 
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginUserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisterUserController;
@@ -10,26 +11,5 @@ Route::resource('jobs', JobController::class);
 //auth
 Route::get('/register',[RegisterUserController::class,'create']);
 Route::post('/register',[RegisterUserController::class,'store']);
-
-//*****************************************************************************
-
-//Route::resource('jobs', JobController::class,[
-//    'only' => ['index', 'show']
-//]);
-
-//Route::resource('jobs', JobController::class,[
-//    'except' => ['create', 'store']
-//]);
-
-//Route::controller(JobController::class)->group(function () {
-//    Route::prefix('/jobs')->group(function () {
-//        Route::get('','index');
-//        Route::get('/create','create');
-//        Route::get('/{job:id}','show');
-//        Route::get('/{job:id}/edit','edit');
-//        Route::post('/','store');
-//        Route::patch('/{job:id}','update');
-//        Route::delete('/{job:id}','destroy');
-//    });
-//});
-
+Route::get('/login',[LoginUserController::class,'create']);
+Route::post('/login',[LoginUserController::class,'store']);
